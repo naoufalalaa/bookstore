df33ae412ab6ab14832e606c8b9032f0a5af725c
Naoufal Alaa <alaanaoufal2000@gmail.com>
2021-12-25 10:22:59 +0100

    v1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\AuteurRepository;
 use App\Repository\GenreRepository;
-
+use App\Repository\LivreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +12,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(AuteurRepository $auteurRepository,LivreRepository $livreRepository,GenreRepository $genreRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'auteurs' => $auteurRepository->findAll(),
+            'livres' => $livreRepository->findAll(),
+            'genres' => $genreRepository->findAll(),
         ]);
     }
-
 }
