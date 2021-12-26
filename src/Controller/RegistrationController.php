e2a03867f94a8fddb7ceccd44e782558d3bd5ba7
Naoufal Alaa <alaanaoufal2000@gmail.com>
2021-12-26 01:39:11 +0100

    commit

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -18,6 +18,10 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
